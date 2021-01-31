0f0e57b14c929f1a39afed1274913afe39702eaa
Rohan kolhatkar <rohankolhatkar930@gmail.com>
2021-01-31 19:52:38 +0530

    Add files via upload

---
+++ 09 avg function.py
@@ -0,0 +1,6 @@
+a= input("enter first number")
+b= input("enter second number")   #as they are strings even if number given so cannot be avg directly
+a= int(a)
+b= int(b)
+avg= (a+b) /2
+print("the average of a and b is : ", avg)
---
+++ variable swap.py
@@ -0,0 +1,8 @@
+x=int(input("enter number x"))
+y=int(input("enter number y"))
+        
+temp =y
+y=x
+x=temp
+print("swapped value of x",x)
+print("swapped value of y",y)
